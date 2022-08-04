aeb9cad8390e2e8c2bb47bc391a04a25c2b63dd0
Daria <daryanickulina@yandex.ru>
2022-08-04 23:15:29 +0300

    feat: adds header visibility js

--- index.js
+++ index.js
@@ -1,8 +1,53 @@
 const body = document.querySelector('.page')
 const header = document.querySelector('.header');
 const purchase = document.querySelector('.purchase');
-// const purchaseWayToWindowTop = purchase.getBoundingClientRect().bottom;
+const purchaseheigth = purchase.getBoundingClientRect().height;
+const purchaseInfo = purchase.querySelector('.purchase__info');
+const purchaseButton = purchase.querySelector('.purchase__button');
+const purchaseList = purchase.querySelector('.purchase__options');
+const purchaseListItems = Array.from(purchaseList.options);
 
+const aa = purchaseList.querySelectorAll('.purchase__option');
+
+// purchaseListItems.forEach((element) => {
+//   element.addEventListener(('click'), () => {
+//     isSelected();
+//     element.setAttribute('selected', 'selected');
+//   })
+// })
+
+// aa.forEach((element) => {
+//   element.addEventListener('click', ff);
+// })
+
+
+// function ff(el) {
+//   console.log(el)
+// }
+
+function getSelectedItem() {
+  let selectedElement = null;
+  const items = Array.from(purchaseList.options);
+  items.forEach((el) => {
+    el.selected ? selectedElement = el : '';
+  })
+  return selectedElement
+}
+
+purchaseList.addEventListener(('change'), (evt) => {
+  debugger
+  isSelected(evt);
+  getSelectedItem();
+  // element.setAttribute('selected', '');
+})
+
+function isSelected(evt) {
+  purchaseListItems.forEach((element) => {
+    if (element.hasAttribute('selected')) {
+      element.removeAttribute('selected');
+    }
+  })
+}
 
 // const selectedItemAdd = document.querySelector('.purchase__new-price');
 // const text = document.querySelector('.purchase__new-price_sticky');
@@ -20,23 +65,46 @@ const purchase = document.querySelector('.purchase');
 // добавиьт ссылку в ревью
 // удалить селект и добавлять его с помощью js
 // ограничить выпадающий список по ширине 
-
 // изменить положение попапа
 
 
-document.addEventListener('scroll', setPurchaseOpen);
+purchaseButton.addEventListener('click', getSelectedItem);
+
+
+
+
+let scroll = 0;
+
+document.addEventListener('scroll', (evt) => {
+  setPurchaseOpen();
+  showHeader();
+});
+
+function showHeader() {
+  if (scroll > window.pageYOffset) {
+    header.classList.add('header_scroll');
+  } if (scroll < window.pageYOffset) {
+    header.classList.remove('header_scroll');
+  }
+  scroll = window.pageYOffset;
+}
 
 function setPurchaseOpen() {
-  if (window.pageYOffset >= purchase.getBoundingClientRect().bottom) {
+  if (window.pageYOffset >= purchase.getBoundingClientRect().bottom + purchaseheigth) {
     purchase.classList.add('purchase_sticky');
-    addsSelectedElementTextToAdd();
     // document.removeEventListener('scroll', setPurchaseOpen);
-  } 
+  }
 }
 
-function addsSelectedElementTextToAdd() {
 
-console.log(445)
-  // selectedItemAdd.classList.add('purchase__new-price_sticky');
-  // text.textContent = text + "55";
+
+
+purchaseInfo.addEventListener('click', showFullPurchaseVertion);
+
+
+function showFullPurchaseVertion() {
+  purchase.classList.remove('purchase_sticky');
 }
+
+
+
